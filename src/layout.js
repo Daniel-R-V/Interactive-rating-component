ca8f0f74675b8428a58d1fb3a8ca3a217f276b3d
Daniel <danirvicente@gmail.com>
2022-06-15 11:49:35 +0200

    change color in button pressed

--- src/layout.js
+++ src/layout.js
@@ -1,25 +1,7 @@
-// const bttn = document.querySelectorAll(".btn");
+let bttn = document.querySelectorAll(".btn");
 
-// const changeColor = () => {
-//     for (let i = 0; i < bttn.length; i++) {
-//         bttn[i].style.color = "red";
-//         break;
-//     }
-// };
-
-// let bttn = document.getElementsByClassName(".btn");
-
-// for (let i = 0; i < bttn.length; i++) {
-//     bttn[i].addEventListener("click", () => {
-//         console.log("entra en la funcion");
-//         bttn.style.color = "red";
-//     });
-// }
-
-let butn = document.getElementsByClassName(".btn");
-
-butn.addEventListener("click", changeColor);
-
-const changeColor = () => {
-    butn.style.color = "red";
-};
+for (let i = 0; i < bttn.length; i++) {
+    bttn[i].addEventListener("click", () => {
+        bttn[i].classList.add("newColor");
+    });
+}
