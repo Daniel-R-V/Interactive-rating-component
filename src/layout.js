5d378f17ba081c7303f0ad0057f281c074b3bcc7
Daniel <danirvicente@gmail.com>
2022-06-18 10:07:04 +0200

    finish challenge

--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,5 @@
-let bttn = document.querySelectorAll("label");
+let bttn = document.querySelectorAll("input");
+let rar = document.querySelector("rad");
 let btn = document.getElementById("btn");
 let evaluation = document.querySelector(".evaluation");
 let thankyou = document.querySelector(".thankyou");
@@ -8,11 +9,11 @@ for (let i = 0; i < bttn.length; i++) {
     bttn[i].addEventListener("click", () => {
         bttn[i].classList.add("newColor");
         bttn[i].style.color = "white";
+        span.innerHTML = bttn[i].value;
     });
 }
 
 btn.addEventListener("click", () => {
     evaluation.style.display = "none";
     thankyou.style.display = "block";
-    bttn.value = span;
 });
