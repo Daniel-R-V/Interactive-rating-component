d675e31c78583256fc54749b2e41864413c5410d
Daniel <danirvicente@gmail.com>
2022-06-18 09:37:09 +0200

    thankyou card

--- src/layout.js
+++ src/layout.js
@@ -1,7 +1,18 @@
-let bttn = document.querySelectorAll(".btn");
+let bttn = document.querySelectorAll("label");
+let btn = document.getElementById("btn");
+let evaluation = document.querySelector(".evaluation");
+let thankyou = document.querySelector(".thankyou");
+let span = document.getElementById("rated");
 
 for (let i = 0; i < bttn.length; i++) {
     bttn[i].addEventListener("click", () => {
         bttn[i].classList.add("newColor");
+        bttn[i].style.color = "white";
     });
 }
+
+btn.addEventListener("click", () => {
+    evaluation.style.display = "none";
+    thankyou.style.display = "block";
+    bttn.value = span;
+});
